a655b63ae8448fd9059d93c13bcf528d64dbd2dd
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-06-05 16:56:55 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vezdecode-js/' : '/',
+  publicPath: '/vezdecode-js/',
   productionSourceMap: false,
   devServer: {
     port: 3000,
